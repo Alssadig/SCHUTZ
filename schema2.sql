ee81df14c0ff2f686ce5082e06cb5fdb090fb9d5
Alssadig <165165561+Alssadig@users.noreply.github.com>
2024-07-23 16:38:58 +0200

    Update schema2.sql

--- schema2.sql
+++ schema2.sql
@@ -1,26 +1,26 @@
-CREATE TABLE Livres (
-    ID_livre INT PRIMARY KEY,
-    Titre VARCHAR(255),
-    Auteur VARCHAR(255), 
-    Annee_publication INT,
-    Quantite INT
+
+CREATE TABLE allees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom CHAR(1) NOT NULL
 );
 
-CREATE TABLE Utilisateurs (
-    ID_utilisateur INT PRIMARY KEY,
-    Nom VARCHAR(255),
-    Prenom VARCHAR(255),
-    Email VARCHAR(255),
-    Date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE emplacements (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    allee_id INT,
+    numero INT,
+    CONSTRAINT fk_allee FOREIGN KEY (allee_id) REFERENCES allees(id)
 );
 
-CREATE TABLE Emprunts (
-    ID_emprunt INT PRIMARY KEY,
-    ID_utilisateur INT,
-    ID_livre INT,
-    Date_emprunt DATE,
-    Date_retour_prevue DATE,
-    Date_retour_effective DATE,
-    FOREIGN KEY (ID_utilisateur) REFERENCES Utilisateurs(ID_utilisateur),
-    FOREIGN KEY (ID_livre) REFERENCES Livres(ID_livre)
+CREATE TABLE composants (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    reference VARCHAR(20) NOT NULL,
+    date_arrivee DATE NOT NULL
+);
+
+CREATE TABLE stockage (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    emplacement_id INT,
+    composant_id INT,
+    CONSTRAINT fk_emplacement FOREIGN KEY (emplacement_id) REFERENCES emplacements(id),
+    CONSTRAINT fk_composant FOREIGN KEY (composant_id) REFERENCES composants(id)
 );
