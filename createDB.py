7500424e42ca0455565f15f4ef36c4e8b1b46498
Alssadig <165165561+Alssadig@users.noreply.github.com>
2024-08-29 16:36:22 +0200

    Create createDB.py

---
+++ createDB.py
@@ -0,0 +1,14 @@
+import sqlite3
+import mysql
+
+connection = sqlite3.connect('schutz.db')
+
+with open('Documents/schema SCHUTZ.sql') as fcreate:
+    connection.executescript(fcreate.read())
+connection.commit()
+
+with open('Documents/init_db_insert.sql') as finsert:
+    connection.executescript(finsert.read())
+connection.commit()
+
+connection.close()
